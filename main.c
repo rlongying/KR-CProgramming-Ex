ef6413256508285604030feabd7a74e6cc5ab25f
Mengjun(Ivan) Wang <wmj900823@gmail.com>
2020-03-28 23:42:33 -0700

    define swap macro

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 //#include "ch3/ch3_test.h"  // exercises for chapter 3
 #include "ch4/ch4_test.h"  // exercises for chapter 4
 
+#define swap(t, x, y) {t tmp = x; x = y, y = tmp;} /* swap x, y of type t */
+
 void test_ch2() {
     //    print_type_range();
 //    test_any();
@@ -30,9 +32,30 @@ void test_ch4() {
     test_reverse2();
 }
 
+
 int main() {
 //    test_ch2();
-    test_ch4();
+//    test_ch4();
+    int ix, iy;
+    double dx, dy;
+    char *px, *py;
 
+    ix = 42;
+    iy = 69;
+    printf("integers before swap: %d and %d\n", ix, iy);
+    swap(int, ix, iy);
+    printf("integers after swap: %d and %d\n", ix, iy);
+
+    dx = 123.0;
+    dy = 321.0;
+    printf("doubles before swap: %g and %g\n", dx, dy);
+    swap(double, dx, dy);
+    printf("integers after swap: %g and %g\n", dx, dy);
+
+    px = "hello";
+    py = "world";
+    printf("pointers before swap: %s and %s\n", px, py);
+    swap(char *, px, py);
+    printf("integers after swap: %s and %s\n", px, py);
     return 0;
 }
