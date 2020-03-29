b155e8fa9b2870fb23d825315e946dec78f0b47e
Mengjun(Ivan) Wang <wmj900823@gmail.com>
2020-03-29 13:40:44 -0700

    add folder for ch5

---
+++ ch5/ch5.h
@@ -0,0 +1,10 @@
+//
+// Created by Mengjun Wang on 2020-03-29.
+//
+
+#ifndef KR_CPROGRAMMING_CH5_H
+#define KR_CPROGRAMMING_CH5_H
+
+
+
+#endif //KR_CPROGRAMMING_CH5_H
---
+++ ch5/exerch5.c
@@ -0,0 +1,5 @@
+//
+// Created by Mengjun Wang on 2020-03-29.
+//
+
+#include "ch5.h"
